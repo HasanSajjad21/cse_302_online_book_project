d16c113eb8977fc3b067325461bbb40289d7289f
HasanSajjad21 <zahidsajjad2121@gmail.com>
2023-05-29 11:52:31 +0600

    icons add

--- js/script.js
+++ js/script.js
@@ -1,23 +1,25 @@
 searchForm = document.querySelector('.search-form');
-document.querySelector('#search-btn').onclick = () =>{
+
+document.querySelector('#search-btn').onclick = () => {
     searchForm.classList.toggle('active');
 
 }
-window.onscroll = () =>{
 
+window.onscroll = () =>{
     searchForm.classList.remove('active');
 
-    if(window.scrollY > 80){
+
+    if (window.scrollY > 80){
         document.querySelector('.header .header-2').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
 
 window.onload = () =>{
-    if(window.scrollY > 80){
+    if (window.scrollY > 80) {
         document.querySelector('.header .header-2').classList.add('active');
-    }else{
+    } else {
         document.querySelector('.header .header-2').classList.remove('active');
     }
 }
